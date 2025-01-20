60916586eff0162ed3c02f0158fba2c43baae3bb
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-01-20 07:09:27 -0800

    Type Inference in for loop

---
+++ src/main/java/dev/sumantakumar/typeinference/ForLoop.java
@@ -0,0 +1,9 @@
+package dev.sumantakumar.typeinference;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(var i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
