94ae9d4568b76b9859a5f28c661cfae82371123d
Sumanta Kumar Sahoo <sumant7654@gmail.com>
2025-01-20 06:33:15 -0800

    Initial Commit

---
+++ src/main/java/dev/sumantakumar/typeinference/TypeInferenceFirstExample.java
@@ -0,0 +1,14 @@
+package dev.sumantakumar.typeinference;
+
+public class TypeInferenceFirstExample {
+
+    public static void main(String[] args) {
+        var roll = 100;
+        var name = "Sumanta Kumar";
+        var percentage = 80.00;
+
+        System.out.println(roll);
+        System.out.println(name);
+        System.out.println(percentage);
+    }
+}
